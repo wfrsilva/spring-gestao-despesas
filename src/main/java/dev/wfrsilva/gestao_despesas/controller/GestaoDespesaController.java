db012166283e9277c36bc166b70d1836a76d04bd
wfrsilva <wfrsilva@gmail.com>
2025-10-15 22:59:58 -0300

    Classe BuscarDespesaUseCase criada
     Changes to be committed:
    	modified:   data/gestao-despesa.mv.db
    	modified:   src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
    	new file:   src/main/java/dev/wfrsilva/gestao_despesas/useCase/BuscarDespesaUseCase.java

--- src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
+++ src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
@@ -1,14 +1,21 @@
 package dev.wfrsilva.gestao_despesas.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.wfrsilva.gestao_despesas.custom_message.ErrorMessage;
+import dev.wfrsilva.gestao_despesas.entity.Despesa;
+import dev.wfrsilva.gestao_despesas.useCase.BuscarDespesaUseCase;
 import dev.wfrsilva.gestao_despesas.useCase.CadastroDespesaUseCase;
 
 @RequestMapping("/gestao")
---
+++ src/main/java/dev/wfrsilva/gestao_despesas/useCase/BuscarDespesaUseCase.java
@@ -0,0 +1,33 @@
+package dev.wfrsilva.gestao_despesas.useCase;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import dev.wfrsilva.gestao_despesas.entity.Despesa;
+import dev.wfrsilva.gestao_despesas.repository.DespesaRepository;
+
+public class BuscarDespesaUseCase {
+
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+    public List <Despesa> buscarPorEmailEData(String email, LocalDate data)
+    {
+        List <Despesa> despesas;
+
+        if(data != null)
+        {
+            despesas = despesaRepository.findByEmailAndData(email, data);
+        }//if
+        else
+        {
+            despesas = despesaRepository.findByEmail(email);
+        }//else
+
+        return despesas;
+
+    }//buscarPorEmailEData
+    
+}//BuscarDespesaUseCase
